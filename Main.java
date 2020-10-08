65065e10d5af1c1bd9129dca673d7adda26692ff
tomad6909 <tomad6909@wrdsb.ca>
2020-10-08 15:05:29 +0000

    finished A6Q2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will populate a 10 element array with integers and have the user enter a number to search for. The program will then search through the array and state the index that the number is at
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // create an array with 10 spots
+    int[] integers = new int[10];
+
+    // ask the user to enter in 10 integers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    // create a for loop and put in integers
+    for(int i = 0; i < 10; i++){
+      integers[i] = input.nextInt();
+    } 
+
+    // ask the user to enter a number to find
+    System.out.println("Please enter a number to find");
+    int numberFind = input.nextInt();
+
+    for(int i = 0; i < integers.length; i++){
+      if(integers[i] == numberFind){
+        System.out.println(numberFind + " is located at index " + i);
+      }else{
+        
+      }
+    }
+
+
     
   }
 }
